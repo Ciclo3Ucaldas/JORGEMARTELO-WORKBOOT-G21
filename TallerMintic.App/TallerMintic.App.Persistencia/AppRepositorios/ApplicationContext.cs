e72aac575ab117555f14137e2460f5099d038dda
jorgePrograma <jemartelo07@gmail.com>
2022-09-09 18:16:58 -0500

    verificar conexion

--- TallerMintic.App/TallerMintic.App.Persistencia/AppRepositorios/ApplicationContext.cs
+++ TallerMintic.App/TallerMintic.App.Persistencia/AppRepositorios/ApplicationContext.cs
@@ -1,23 +1,22 @@
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using TallerMintic.App.Dominio.Entidades;
 
 namespace TallerMintic.App.Persistencia.AppRepositorios
 {
-    public class ApplicationContext:DbContext
+    public class ApplicationContext : DbContext
     {
-     public DbSet<Cliente> Clientes{get;set;}  
+
+        private const string conexion = @"Data Source=localhost\sqlexpress;Initial Catalog=BbTallerMintic; Integrated Security=True;";
+        public DbSet<Cliente> Clientes { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
             {
-                optionsBuilder.UseSqlServer("Data Source=(localdb)//MSSQLLocalDB Catalog=HopitalEncasa");
+                // optionsBuilder.UseSqlServer("Data Source=(localdb)//MSSQLLocalDB Catalog=HopitalEncasa");
+                optionsBuilder.UseSqlServer(conexion);
             }
-        } 
+        }
     }
 
 }
