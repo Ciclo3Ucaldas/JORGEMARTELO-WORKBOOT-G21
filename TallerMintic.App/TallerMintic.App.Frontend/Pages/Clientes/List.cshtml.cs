ba1c734a15922ce648af9bc18f49ce85d2d8377b
jorgePrograma <jemartelo07@gmail.com>
2022-09-10 16:31:31 -0500

    vista cliente listar

--- TallerMintic.App/TallerMintic.App.Frontend/Pages/Clientes/List.cshtml.cs
+++ TallerMintic.App/TallerMintic.App.Frontend/Pages/Clientes/List.cshtml.cs
@@ -1,12 +1,22 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using TallerMintic.App.Dominio.Entidades;
+using TallerMintic.App.Persistencia.AppRepositorios;
 
 namespace TallerMintic.App.Frontend.Pages
 {
     public class ListModel : PageModel
     {
+      
+        private readonly IRepositorioCliente repositorioCliente;
+        public IEnumerable<Cliente> clientes { get; set; }
+        public ListModel(IRepositorioCliente repositorioCliente)
+        {
+            this.repositorioCliente = repositorioCliente;
+        }
         public void OnGet()
         {
+            clientes=repositorioCliente.GetAllClientes();
         }
     }
 }
