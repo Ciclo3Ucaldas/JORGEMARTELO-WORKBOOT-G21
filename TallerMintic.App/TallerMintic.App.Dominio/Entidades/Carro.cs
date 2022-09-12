0b5ba038f71ead6ba696f424a027f2a57a22d5bb
JorgePrograma <jemartelo07@gmail.com>
2022-09-12 09:27:22 -0500

    subida entidades

---
+++ TallerMintic.App/TallerMintic.App.Dominio/Entidades/Carro.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JORGEMARTELO-WORKBOOT-G21
+{
+    public class Carro
+    {
+        
+    }
+}
