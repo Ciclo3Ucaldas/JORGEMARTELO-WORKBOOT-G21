ccb44b1fa7241664412cda38e44e6c8bcd72a0dc
JorgePrograma <jemartelo07@gmail.com>
2022-09-14 13:02:20 -0500

    fffff

--- proyecto/ContosoUniversity/ContosoUniversity.TallerMecanico/Pages/Students/Index.cshtml.cs
+++ proyecto/ContosoUniversity/ContosoUniversity.TallerMecanico/Pages/Students/Index.cshtml.cs
@@ -1,19 +1,24 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using ContosoUniversity.Dominio.Models;
+using System;
+using System.Collections.Generic;
 using ContosoUniversity.Persistencia.Data;
+using ContosoUniversity.Persistencia.Repositorios;
+using ContosoUniversity.Dominio.Models;
 
 namespace MyApp.Namespace
 {
     public class IndexModel : PageModel
     {
-        private IStudentRepository studentRepository;
-        public StudentController()
+        private readonly IStudentRepository repositorio;
+        public IEnumerable<Student> students { get; set; }
+        public IndexModel(IStudentRepository repository)
         {
-            this.studentRepository = new StudentRepository(new SchoolContext());
+            this.repositorio = repository;
         }
         public void OnGet()
         {
+            students.repositorio.GetStudents();
         }
     }
 }
