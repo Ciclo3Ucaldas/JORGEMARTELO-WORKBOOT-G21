a15d9f9cd1a1328956b47e94407ead7cc2135f3f
JorgePrograma <jemartelo07@gmail.com>
2022-09-14 22:26:26 -0500

    actualizar y eliminar student

--- proyecto/ContosoUniversity/ContosoUniversity.TallerMecanico/Pages/Students/Create.cshtml.cs
+++ proyecto/ContosoUniversity/ContosoUniversity.TallerMecanico/Pages/Students/Create.cshtml.cs
@@ -10,49 +10,19 @@ namespace MyApp.Namespace
     public class CreateModel : PageModel
     {
         private readonly IStudentRepository _repositorio;
-       // public Student students { get; set; }
+        public Student Student { get; set; }
         public CreateModel(IStudentRepository repository)
         {
             this._repositorio = repository;
         }
         public void OnGet()
         {
-        /*    Console.WriteLine("Agregando 1");
-            Student estudiante = new Student();
-            estudiante.LasName = "Karla";
-            estudiante.FirstMidName = "Gonzales";
-            estudiante.EnrollmentDate = DateTime.Parse("2022-09-15");
-            _repositorio.InsertStudent(estudiante);
-            _repositorio.Save();
-            Console.WriteLine("Exitoso");
-            
-            Console.WriteLine("Agregando 2");
-            Student estudiante2 = new Student();
-            estudiante2.LasName = "Maria";
-            estudiante2.FirstMidName = "Gutierrez";
-            estudiante2.EnrollmentDate = DateTime.Parse("2022-09-14");
-            _repositorio.InsertStudent(estudiante2);
-            _repositorio.Save();
-            Console.WriteLine("Exitoso");
-            
-            Console.WriteLine("Agregando 3");
-            Student estudiante3 = new Student();
-            estudiante3.LasName = "Juan";
-            estudiante3.FirstMidName = "Gonzales";
-            estudiante3.EnrollmentDate = DateTime.Parse("2022-09-15");
-            _repositorio.InsertStudent(estudiante3);
-            _repositorio.Save();
-            Console.WriteLine("Exitoso");
-            
-            Console.WriteLine("Agregando 4");
-            Student estudiante4 = new Student();
-            estudiante4.LasName = "Alfredo";
-            estudiante4.FirstMidName = "Martinez";
-            estudiante4.EnrollmentDate = DateTime.Parse("2022-09-15");
-            _repositorio.InsertStudent(estudiante4);
-            _repositorio.Save();
-            Console.WriteLine("Exitoso");
-       */ }
+        }
 
+        public IActionResult OnPost(Student student)
+        {
+            _repositorio.InsertStudent(student);
+            return new RedirectToPageResult("./Index");
+        }
     }
 }
--- proyecto/ContosoUniversity/ContosoUniversity.TallerMecanico/Pages/Students/Delete.cshtml.cs
+++ proyecto/ContosoUniversity/ContosoUniversity.TallerMecanico/Pages/Students/Delete.cshtml.cs
@@ -1,12 +1,31 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using ContosoUniversity.Persistencia.Data;
+using ContosoUniversity.Persistencia.Repositorios;
+using ContosoUniversity.Dominio.Models;
 
 namespace MyApp.Namespace
 {
     public class DeleteModel : PageModel
     {
-        public void OnGet()
+
+        private readonly IStudentRepository _repository;
+        public Student Student { get; set; }
+        public DeleteModel(IStudentRepository repository)
         {
+            _repository = repository;
+        }
+        public void OnGet(int id)
+        {
+            Student = _repository.GetStudentByID(id);
+        }
+        public IActionResult OnPost(int id)
+        {
+            _repository.DeleteStudent(id);
+            _repository.Save();
+            return new RedirectToPageResult("./Index");
         }
     }
 }
